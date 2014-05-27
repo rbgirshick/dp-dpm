e0e94490a3d24fbb5fce8ffa9e3d98db97f8825e
Ross Girshick <rbg@eecs.berkeley.edu>
2014-05-27 10:34:17 -0700

    streamlined training procedure

--- train/pascal_train_mixture.m
+++ train/pascal_train_mixture.m
@@ -46,33 +46,19 @@ neg_small = neg_perm(1:min(num_neg, conf.training.num_negatives_small));
 %  neg_small(i).dataid = neg_small(i).dataids + 100000;
 %end
 
-save_file = [cachedir cls '_init'];
-try
-  load(save_file);
-catch
-  seed_rand();
-  for i = 1:n
-    models{i} = root_model(cls, spos{i}, note);
-    % Get warped positives and random negatives
-    models{i} = train(models{i}, spos{i}, neg_small, true, true, 1, 1, ...
-        max_num_examples, fg_overlap, 0, false, 'init');
-    models{i} = train(models{i}, spos{i}, neg_small, false, false, 1, 10, ...
-        max_num_examples, fg_overlap, num_fp, true, 'hard_neg');
-    models{i} = train(models{i}, spos{i}, neg_small, false, false, 1, 10, ...
-        max_num_examples, fg_overlap, num_fp, false, 'hard_neg');
-  end
-  save(save_file, 'models');
+for i = 1:n
+  models{i} = root_model(cls, spos{i}, note);
 end
+model = model_merge(models);
 
 save_file = [cachedir cls '_final'];
 try
   load(save_file);
 catch
-  model = model_merge(models);
   model = model_cnn_init(model);
-  model = train(model, impos, neg_small, false, false, 1, 10, ...
-                max_num_examples, fg_overlap, num_fp, false, 'hard_neg');
+  model = train(model, impos, neg_small, true, false, 1, 10, ...
+                max_num_examples, fg_overlap, num_fp, false, 'hard_neg1');
   model = train(model, impos, neg, false, false, 1, 10, ...
-                max_num_examples, fg_overlap, num_fp, true, 'hard_neg');
+                max_num_examples, fg_overlap, num_fp, false, 'hard_neg2');
   save(save_file);
 end
